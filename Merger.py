9305df4f1c3a5ab71b3ba8590ce95cdb845fdf5a
cwallace3 <47311101+cwallace3@users.noreply.github.com>
2019-05-20 14:02:07 -0400

    Formatting - delete spaces between lines

--- Merger.py
+++ Merger.py
@@ -1,11 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Helpful websites used in this script:
 # https://stackoverflow.com/questions/11709079/parsing-html-using-python
-
 # https://www.guru99.com/reading-and-writing-files-in-python.html
-
-
 # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#quick-start
 
 with open('TFC_Character_Submissions.html') as submit:
@@ -23,16 +21,13 @@ with open('TFC_Character_Submissions.html') as submit:
         data = resp.text
 
         f = open('all_submissions2.txt', 'a+')
-#  https://www.guru99.com/reading-and-writing-files-in-python.html
 # " "a" for append and plus sign means if it is not there then create it "
-        
-        
+                
         print (data)
         
         for x in data.split('\n'):
             print(data)
-            f.write(data)        
-        
+            f.write(data)                
               
 f.close()
         
